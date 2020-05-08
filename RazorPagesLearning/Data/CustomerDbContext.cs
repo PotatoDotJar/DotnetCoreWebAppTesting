f5a5c3dc15e6b244c5946957c52a012b38b39610
RJ <rmnader@svsu.edu>
2020-05-08 17:13:08 -0400

    Added razor test project.

---
+++ RazorPagesLearning/Data/CustomerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesLearning.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesLearning.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ RazorPagesLearning/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using RazorPagesLearning.Data;
+using RazorPagesLearning.Data.Models;
+
+namespace RazorPagesLearning.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly CustomerDbContext _context;
+
+
+        public IndexModel(ILogger<IndexModel> logger, CustomerDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IList<Customer> Customer { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Customer = await _context.Customers.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var contact = await _context.Customers.FindAsync(id);
+
+            if (contact != null)
+            {
+                _context.Customers.Remove(contact);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
